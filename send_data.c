d969fea39da0ab731f433cb8926291e7e444337d
Yaya13 <59097388+Tiny-Rick13@users.noreply.github.com>
2022-11-16 11:04:28 +0100

    Update send_data.c

--- send_data.c
+++ send_data.c
@@ -1,62 +1,83 @@
-// #include "C:\Pico\pico-sdk\src\rp2_common\pico_stdlib\stdlib.h"
-#include "pico/stdio.h"
-#include <serial>
-// #include "C:\Pico\pico-sdk\src\rp2_common\hardware_irq\include\hardware\irq.h"
-// #include "pico/multicore.h"
-// #include "hardware/structspio.h"
-#include "C:\Pico\pico-sdk\src\rp2040\hardware_regs\include\hardware\regs\uart.h"
-// #include "uart_rx.pio.h"
+#include <Arduino.h>
+#include "C:\COM2\lib\.platformio\packages\framework-arduino-mbed\cores\arduino\Serial.h"
+#include "C:\COM2\lib\.platformio\packages\framework-arduino-mbed\cores\arduino\api\cstdio"
+#include <uart.h>
+#include <stdio_uart.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define UART_ID uart0
-int BAUD_RATE 115200;
-#define DATA_BITS 8 // taille constante de la trame
-#define START_BITS 1 // bit alloué pour informer du début de la trame
-#define PARITY    UART_PARITY_NONE // jsp à quoi ça sert mais bon
-int UART_TX_PIN 6;
-int UART_RX_PIN 7;
+uint BAUD_RATE = 112500; //vitesse de communication de la pico en usb 
+uart_inst_t UART_ID = uart0;
 
-stdio_uart_init_full(UART_ID *uart, int BAUD_RATE, UART_TX_PIN, UART_RX_PIN);
+int DATA_BITS = 8; //taille de la trame 
+int START_BIT = 0; //bit de départ (provisoire)
 
-char buffer_reception[8]; //byte de taille 8 : jusqu'à 255
-// uart_init(UART_ID, BAUD_RATE); // je comprends pas l'erreur
+int A = 27; //contrôle vitesse moteur 1
+int B = 26; //contrôle direction moteur 1
+
+int codeuseA = 25;
+int codeuseB = 24; 
+int UART_TX_PIN = 6;
+int UART_RX_PIN = 7;
+
+uart_init(UART_ID, BAUD_RATE);
+
+void setup() {
+    // Initialise entrées/sortie
+    pinMode(codeuseA, INPUT_PULLUP);
+    pinMode(codeuseB, INPUT_PULLUP);
+
+    pinMode(A, OUTPUT);
+    pinMode(B, OUTPUT);
+}
+
+// Définition des fonctions 
+
+void stop() {
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+}
+
+void avancer(char a) {
+    analogWrite(A, a); //contrôle vitesse en PMW (tension en créneau)
+    digitalWrite(B, HIGH);
+}
+
+void reculer(char a) {
+    analogWrite(A, a); //contrôle vitesse en PMW (tension en créneau)
+    digitalWrite(B, LOW);
+}
 
 int main() {
-
-    //Initialise I/O
-    stdio_init_all(); 
-
-    //Main Loop 
-    while(1) {
+    
+    while (1) {
+        char buffer_reception[DATA_BITS]; //stocke la trame reçue, byte de taille 8 : 255 possibilités
         
-
-        if(uart_is_readable(UART_ID)) // vérifie que l'UART a fini sa phase d'envoi
-  { 
-    Serial.readBytes(buffer_reception, 8); // lit 8 bits de données envoyés par l'USB et les stock dans le buffer
-
-    if (buffer_reception[0] == 0) // '0' est le caractère spécial provisoire annoncant le début d'un ordre.
-      {
-        printf("Début de l'ordre");
-      
-      for( int i = 1; i < 8; i ++ ) { // on lit chaque bit sauf le bit de début
-
-        switch (buffer_reception[i]) { // chaque bit correspond à un ordre en fonction du caractère.
-          case 1: 
-             uart_puts(uart0, "ORDRE1"); // écrit à l'UART pour la transmission, la fonction bloque jusqu'à que tout soit envoyé.
-             break;
-          case 2:
-             uart_puts(uart0, "ORDRE2");
-             break;
-          default:
-             uart_puts(uart0, "Code d'erreur"); // si le bit ne correspond à aucun ordre -> erreur
-             break;   
-
+        if (uart_is_readable(UART_ID) != 0) {
+            
+            uart_read_blocking(UART_ID, buffer_reception, DATA_BITS);
+            
+            if (buffer_reception[0] == START_BIT) { // Le programme ne fonctionne que si le 1er bit est le bit de départ
+               
+                printf("Début de l'ordre");
+                
+                for( int i; i < DATA_BITS; i ++ ) { // On lit chaque bit reçu
+                   
+                    switch(buffer_reception[i]) {
+                        case 1 : 
+                            uart_puts( UART_ID, "ORDRE 1");
+                            break;
+                        case 2 :
+                            uart_puts( UART_ID, "ORDRE 2");
+                            break; //etc jusqu'à 8 
+                        default : //si le bit ne correspond à aucun ordre, renvoyer une erreur
+                            uart_puts( UART_ID, "CODE D'ERREUR");
+                            break;
+                    }
+                }
+            
+            }
         }
-      
-          
-      }
-      
-
-    Serial.flush(); // attend que la transmission se finisse
-  }
-}
+       
     }
+}
